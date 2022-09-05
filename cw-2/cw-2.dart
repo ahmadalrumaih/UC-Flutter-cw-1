1224d36e2f170eefdc5cade0e80baf2d07db6987
ahmad <ahmadq8200055@gmail.com>
2022-09-05 19:40:58 +0300

    done

---
+++ cw-2/cw-2.dart
@@ -0,0 +1,18 @@
+void main() {
+   
+int height = 180;
+double weight = 65.5;  
+double bmi = (weight) / (height * height);
+  
+  if (bmi < 18.6) {
+  print("underweight");
+    
+  } else if (bmi < 24.9) {
+  print("underweight");
+    
+  } else if (bmi > 24.9) {
+  print("Normal");
+  }
+  
+  
+}
